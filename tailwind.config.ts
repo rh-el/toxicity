33946d23c7e6a9da47f33cad05834958523d360e
williampetitpierre <williampp@hotmail.fr>
2024-12-03 11:57:34 +0100

    added: login page, style, tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,21 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+	fontFamily: {
+		'quicksand': ['Quicksand'],
+		'inter': ['Inter']
+	},
+  	extend: {
+  		colors: {
+			'primary': '#2D316F'
+  		},
+  	}
   },
-  plugins: [],
 } satisfies Config;

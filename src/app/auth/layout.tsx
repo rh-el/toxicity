7c0d1a27f76b4b1c5b353d4cebad6dc69311f428
annekaftal <anne.kaftal@gmail.com>
2024-12-05 14:48:01 +0100

    Add: Post interface

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,12 +1,12 @@
 export default function AuthLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
     <div className="flex items-center flex-col justify-center h-full">
-        <h1 className="font-quicksand text-5xl pt-12 top-0 absolute">weett</h1>
-        {children}
+      <h1 className="font-quicksand text-5xl pt-12 top-0 absolute">weett</h1>
+      <div className="main-container">{children}</div>
     </div>
-    );
-  }
+  );
+}
--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { posts } from "@prisma/client";
+import Image from "next/image";
 import { useEffect, useState } from "react";
+
+interface Post extends posts {
+  users: { id: bigint; username: string; avatar: string };
+}
 
 export default function Home() {
   const [postData, setPostData] = useState<[]>();
@@ -24,8 +29,14 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col gap-3 w-full">
-        {postData?.map((post: posts) => (
+        {postData?.map((post: Post) => (
           <div key={post.id} className="card card-post">
+            <Image
+              width={500}
+              height={500}
+              src={post.users.avatar}
+              alt=""
+            ></Image>
             {post.content}
           </div>
         ))}

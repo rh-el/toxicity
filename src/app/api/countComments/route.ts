e0a0b2eddc91c7d10aa09db638321e62748f1277
annekaftal <anne.kaftal@gmail.com>
2024-12-05 17:13:49 +0100

    Add: comment count

---
+++ src/app/api/countComments/route.ts
@@ -0,0 +1,31 @@
+import {NextResponse} from "next/server"
+import dotenv from 'dotenv'
+import { headers } from 'next/headers'
+import prisma from "@/lib/db/prisma";
+dotenv.config()
+  
+declare global {
+    interface BigInt {
+        toJSON(): number;
+    }
+}
+BigInt.prototype.toJSON = function () { return Number(this) }
+
+export async function GET(req: Request) {
+
+    try {    
+        const headersList = await headers()
+        const postId = headersList.get('post_id')
+        const parsedId = parseInt(postId!)
+
+
+        const commentCount = await prisma.comments.count({
+        where: {
+            post_id: parsedId
+        }
+        })
+            return NextResponse.json(commentCount);
+    } catch (error) {
+        console.error(error)
+    }
+}

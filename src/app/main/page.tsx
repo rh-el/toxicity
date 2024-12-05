b1f5c7d861c3908ec15ac19c288acac6979f2740
annekaftal <anne.kaftal@gmail.com>
2024-12-05 15:25:27 +0100

    Add: affichage des posts sur le composant Home

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -4,6 +4,10 @@ import { posts } from "@prisma/client";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
+const imageLoader = (src: string, width?: number, quality?: number) => {
+  return `${src}?w=${width}&q=${quality}`;
+};
+
 interface Post extends posts {
   users: { id: bigint; username: string; avatar: string };
 }
@@ -32,12 +36,19 @@ export default function Home() {
         {postData?.map((post: Post) => (
           <div key={post.id} className="card card-post">
             <Image
-              width={500}
-              height={500}
+              loader={() => imageLoader(post.users.avatar)}
+              width={40}
+              height={40}
               src={post.users.avatar}
               alt=""
+              priority
+              className="rounded-full"
             ></Image>
-            {post.content}
+            <div className="flex flex-col gap-2">
+              <h1 className="font-semibold pt-2">{post.users.username}</h1>
+              {post.content} Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Nam nulla consequatur, earum nostrum corrupti nesciunt
+            </div>
           </div>
         ))}
       </div>

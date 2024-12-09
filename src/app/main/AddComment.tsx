fc6f829424b009f88e1ee662072c6e2f183e18ba
annekaftal <anne.kaftal@gmail.com>
2024-12-09 12:26:42 +0100

    Add: comment input box

--- src/app/main/AddComment.tsx
+++ src/app/main/AddComment.tsx
@@ -12,29 +12,59 @@ const imageLoader = (src: string, width?: number, quality?: number) => {
 };
 
 interface userType {
-  id: bigint;
   username: string;
   avatar: string;
+  bio: string;
 }
 
-type Props = {
-  user: userType;
-};
+const AddComment = () => {
+  const [userData, setUserData] = useState<userType>([]);
 
-const AddComment = ({ user }: Props) => {
+  const getCommenterInfo = async () => {
+    try {
+      const response = await fetch(`/api/profile`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${Cookies.get("token")}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Error while fetching next post");
+      }
+      const data = await response.json();
+
+      setUserData(data.profileData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getCommenterInfo();
+  }, []);
+
   return (
-    <div className="comment-card card-post">
-      <Image
-        loader={() => imageLoader(user.avatar)}
-        height={50}
-        width={50}
-        src={user.avatar}
-        alt=""
-        priority
-        className="rounded-full"
-      ></Image>
-      <div className="flex flex-col gap-2 pb-3">
-        <h1 className="font-semibold pt-2">{user.username}</h1>
+    <div className=" card-post">
+      <div className="flex gap-3 w-full">
+        <div className="w-16">
+          <Image
+            loader={() => imageLoader(userData.avatar)}
+            height={50}
+            width={50}
+            src={userData.avatar}
+            alt=""
+            priority
+            className="rounded-full"
+          ></Image>
+        </div>
+        <div className="flex flex-col gap-2 pb-3 w-full">
+          <h1 className="font-semibold pt-2">{userData.username}</h1>
+          <input
+            type="text-area"
+            placeholder="Enter your comment"
+            className="bg-white/30 h-8 font-light p-1 text-sm text-start focus:ring-transparent focus:border focus:border-white placeholder-gray-400 rounded-sm"
+          />
+        </div>
       </div>
     </div>
   );

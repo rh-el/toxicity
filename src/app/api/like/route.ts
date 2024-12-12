7ee56a1dfb8e6692547c7e58e2ae675c940fc2a8
annekaftal <anne.kaftal@gmail.com>
2024-12-12 17:02:37 +0100

    Fix: Like

--- src/app/api/like/route.ts
+++ src/app/api/like/route.ts
@@ -1,23 +1,19 @@
 import prisma from "@/lib/db/prisma";
 import { NextResponse } from "next/server";
+import { verifyToken } from "@/app/actions/actions";
 
 export async function POST(req: Request) {
 
     try {
         
         const body = await req.json()
-        const token  = req.headers.get('authorization')?.split(' ')[1]    
-        const userIdFetch = await fetch('http://localhost:3000/api/token', {
-            headers: {
-                authorization: `Bearer ${token}`
-            },
-        })
-        const userId = await userIdFetch.json()
+        const token  = req.headers.get('authorization')?.split(' ')[1]   
+        const userId = Number(verifyToken(token))
 
         const like = await prisma.likes.create({
             data: {
               post_id: body.post_id,
-              user_id: userId.tokenCheck
+              user_id: userId
             },
         })
 
--- src/app/main/Like.tsx
+++ src/app/main/Like.tsx
@@ -1,142 +1,155 @@
-import React, { useCallback } from 'react';
-import { useRef, useState, useEffect } from 'react';
-import gsap from 'gsap'
-import { useGSAP } from '@gsap/react';
+import React, { useCallback } from "react";
+import { useRef, useState, useEffect } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import Cookies from "js-cookie";
 
 type LikeStatusType = {
-    postIsLike: boolean;
-    likeCount: number
-  }
+  postIsLike: boolean;
+  likeCount: number;
+};
 
 type Props = {
-    postId: bigint | number,
-    // likeStatus: LikeStatusType
-    // setLikeStatus: Dispatch<SetStateAction<LikeStatusType>>
-    // handleLike: (id: bigint) => void
-}
+  postId: bigint | number;
+  // likeStatus: LikeStatusType
+  // setLikeStatus: Dispatch<SetStateAction<LikeStatusType>>
+  // handleLike: (id: bigint) => void
+};
 
 const Like = ({ postId }: Props) => {
-    const container = useRef<HTMLDivElement | null>(null)
-    const likeClass = `like-icon-${postId}`
-    const [ likeStatus, setLikeStatus ] = useState<LikeStatusType>({postIsLike: false, likeCount: 0})
-    const { contextSafe } = useGSAP();
-    const colorLike = contextSafe(
-        (
-        color: string,
-        scale: number
-        ) => {
-            gsap.from(`.${likeClass}`, {
-                duration: 0.4,
-                scale: scale,
-            })
-            gsap.to(`.${likeClass}`, {
-                duration: 0.4,
-                fill: color,
-                delay: 0.1,          
-            })
+  const container = useRef<HTMLDivElement | null>(null);
+  const likeClass = `like-icon-${postId}`;
+  const [likeStatus, setLikeStatus] = useState<LikeStatusType>({
+    postIsLike: false,
+    likeCount: 0,
+  });
+  const { contextSafe } = useGSAP();
+  const colorLike = contextSafe((color: string, scale: number) => {
+    gsap.from(`.${likeClass}`, {
+      duration: 0.4,
+      scale: scale,
+    });
+    gsap.to(`.${likeClass}`, {
+      duration: 0.4,
+      fill: color,
+      delay: 0.1,
+    });
+  });
+
+  const initColor = contextSafe((color: string) => {
+    gsap.to(`.${likeClass}`, {
+      fill: color,
+      duration: 0.4,
+    });
+  });
+
+  const getLikeStatus = useCallback(
+    async (postId: bigint) => {
+      const strId = String(postId);
+      const response = await fetch("/api/like-status", {
+        headers: {
+          authorization: `Bearer ${Cookies.get("token")}`,
+          post_id: strId,
+        },
+      });
+      const likeData = await response.json();
+      setLikeStatus({
+        postIsLike: !!likeData.isLikedByUser,
+        likeCount: likeData.likeCount,
+      });
+
+      if (likeData.isLikedByUser) {
+        initColor("#b2ffd8");
+      } else {
+        initColor("#000000");
+      }
+    },
+    [initColor]
+  );
+
+  const likePost = async (postId: bigint) => {
+    const response = await fetch("/api/like", {
+      method: "POST",
+      headers: {
+        authorization: `Bearer ${Cookies.get("token")}`,
+      },
+      body: JSON.stringify({
+        post_id: postId,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error("an error occured while liking post");
+    }
+  };
+
+  const unlikePost = async (postId: bigint) => {
+    const response = await fetch("/api/unlike", {
+      method: "POST",
+      headers: {
+        authorization: `Bearer ${Cookies.get("token")}`,
+      },
+      body: JSON.stringify({
+        post_id: postId,
+      }),
     });
 
-    const initColor = contextSafe((color: string) => {
-        gsap.to(`.${likeClass}`, {
-            fill: color,
-            duration: 0.4
-        })
-    })
-
-    const getLikeStatus = useCallback(
-        async (postId: bigint) => {
-            const strId = String(postId)
-              const response = await fetch('/api/like-status', {
-              headers: {
-                authorization: `Bearer ${Cookies.get('token')}`,
-                post_id: strId
-              }
-              })
-              const likeData = await response.json()
-              setLikeStatus({
-                postIsLike: !!likeData.isLikedByUser, 
-                likeCount: likeData.likeCount
-            })
-      
-              if (likeData.isLikedByUser) {
-                initColor('#b2ffd8')
-              } else {
-                initColor('#000000')
-              }
-          }, []
-    ) 
-
-    const likePost = async (postId: bigint) => {
-        const response = await fetch('/api/like', {
-            method: "POST",
-            headers: {
-                authorization: `Bearer ${Cookies.get('token')}`
-              },
-            body: JSON.stringify({
-                'post_id': postId
-            })
-        })
-        if (!response.ok) {
-            throw new Error("an error occured while liking post");
-        }
+    if (!response.ok) {
+      throw new Error("an error occured while unliking post");
     }
+  };
 
-    const unlikePost = async (postId: bigint) => {
-        const response = await fetch('/api/unlike', {
-            method: "POST",
-            headers: {
-                authorization: `Bearer ${Cookies.get('token')}`
-              },
-            body: JSON.stringify({
-                'post_id': postId
-            })
-        })
-
-        if (!response.ok) {
-            throw new Error("an error occured while unliking post");
-        }
+  const handleLikeColor = () => {
+    setLikeStatus((prev) => ({
+      postIsLike: !prev.postIsLike,
+      likeCount: prev.postIsLike ? prev.likeCount - 1 : prev.likeCount + 1,
+    }));
+    if (!likeStatus.postIsLike) {
+      colorLike("#b2ffd8", 0.5);
+      // setLikeStatus({postIsLike: !likeStatus.postIsLike, likeCount: likeStatus.likeCount +1})
+    } else {
+      colorLike("#000000", 1);
+      // setLikeStatus({postIsLike: !likeStatus.postIsLike, likeCount: likeStatus.likeCount -1})
     }
+  };
 
-    const handleLikeColor = () => {
-        setLikeStatus(prev => ({
-            postIsLike: !prev.postIsLike, 
-            likeCount: prev.postIsLike ? prev.likeCount - 1 : prev.likeCount + 1
-        }))
-        if (!likeStatus.postIsLike) {
-            colorLike('#b2ffd8', 0.5) 
-            // setLikeStatus({postIsLike: !likeStatus.postIsLike, likeCount: likeStatus.likeCount +1})
-        } else {
-            colorLike('#000000', 1)
-            // setLikeStatus({postIsLike: !likeStatus.postIsLike, likeCount: likeStatus.likeCount -1})
-        }
+  const handleLike = async () => {
+    if (!likeStatus.postIsLike) {
+      await likePost(postId as bigint);
+    } else {
+      await unlikePost(postId as bigint);
     }
+    handleLikeColor();
+  };
 
-    const handleLike = async () => {
-        if (!likeStatus.postIsLike) {
-            await likePost(postId as bigint)
-        } else {
-            await unlikePost(postId as bigint)
-        }
-        handleLikeColor()
-    }
+  useEffect(() => {
+    getLikeStatus(postId as bigint);
+  }, [getLikeStatus, postId]);
 
-    useEffect(() => {
-        getLikeStatus(postId as bigint)
-    }, [getLikeStatus, postId])
+  return (
+    <>
+      <div className="flex items-center justify-center" ref={container}>
+        <svg
+          width="25"
+          height="25"
+          viewBox="0 0 25 25"
+          className={likeClass}
+          onClick={handleLike}
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M10.9932 3.13581C8.9938 0.798402 5.65975 0.169642 3.15469 2.31001C0.64964 4.45038 0.29697 8.029 2.2642 10.5604C3.89982 12.6651 8.84977 17.1041 10.4721 18.5408C10.6536 18.7016 10.7444 18.7819 10.8502 18.8135C10.9426 18.8411 11.0437 18.8411 11.1361 18.8135C11.2419 18.7819 11.3327 18.7016 11.5142 18.5408C13.1365 17.1041 18.0865 12.6651 19.7221 10.5604C21.6893 8.029 21.3797 4.42787 18.8316 2.31001C16.2835 0.192162 12.9925 0.798402 10.9932 3.13581Z"
+            stroke="black"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </div>
+      <p className="pr-2">{likeStatus.likeCount}</p>
+    </>
+  );
+};
 
-
-
-    return (
-        <>
-        <div className="flex items-center justify-center" ref={container} >
-            <svg width="25" height="25" viewBox="0 0 25 25"  className={likeClass}  onClick={handleLike} xmlns="http://www.w3.org/2000/svg" >
-                <path fillRule="evenodd" clipRule="evenodd" d="M10.9932 3.13581C8.9938 0.798402 5.65975 0.169642 3.15469 2.31001C0.64964 4.45038 0.29697 8.029 2.2642 10.5604C3.89982 12.6651 8.84977 17.1041 10.4721 18.5408C10.6536 18.7016 10.7444 18.7819 10.8502 18.8135C10.9426 18.8411 11.0437 18.8411 11.1361 18.8135C11.2419 18.7819 11.3327 18.7016 11.5142 18.5408C13.1365 17.1041 18.0865 12.6651 19.7221 10.5604C21.6893 8.029 21.3797 4.42787 18.8316 2.31001C16.2835 0.192162 12.9925 0.798402 10.9932 3.13581Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
-        </div>
-        <p className="pr-2">{likeStatus.likeCount}</p>
-        </>
-    )
-}
-
-export default Like
+export default Like;
